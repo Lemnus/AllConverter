58df9e34bdeea064efe223ada0e2ac0ee670dd08
sebisarb <sebisarb@yahoo.com>
2018-11-01 19:43:06 +0200

    Added partial TenToN conversion, full NToTen conversion, improved Exception handling, started negative number handling

--- TheAllConverter/Program.cs
+++ TheAllConverter/Program.cs
@@ -9,6 +9,8 @@ namespace TheAllConverter
 
         //Console.Beep(300, 500);
 
+        static char prefSeparator = '.';
+
         static void Main(string[] args)
         {
             string[] splits;
@@ -18,7 +20,7 @@ namespace TheAllConverter
             int endingBase=10;
             bool goOn = true;
             string input;
-            char prefSeparator = '.';
+            
             int separatorPoz = 4;
             bool isNegative = false;
 
@@ -39,8 +41,8 @@ namespace TheAllConverter
                                " value for that specific field will be reused. \n \n", 0); 
             } // User usage instructions
 
-                                                                        // TODO: Add support for numbers in bases bigger than 10
-
+                                                                            // NI : Check if either base input is in correct format                              
+                                                                            // NI : Printing negative numbers
             do
             {
                 {
@@ -49,6 +51,7 @@ namespace TheAllConverter
                     Console.Write("\n \n");
                 }
                 input = Console.ReadLine();
+                input=input.ToLower();
                                                         
                 if (!String.IsNullOrWhiteSpace(input))
                 {
@@ -84,20 +87,29 @@ namespace TheAllConverter
                         prefSeparator = ' ';
                     }
                 }
-
-                
-                Console.WriteLine("ToN: "+TenToN(number,2));           // TODO: Add Exception handling
-
+                           
                 if (number[0] == '0' && decimals[0]=='0')
                         return;
+
+                Console.WriteLine("NToTen() prints: "+NToTen(number,decimals,2));
+
+
+
                 {
                     ConsolePrinter("Please write a ", 4);
                     ConsolePrinter("starting base ", 2);
                     Console.Write("\n \n");
                 }
                 input = Console.ReadLine();
+                input=input.ToLower();
                 if (!String.IsNullOrWhiteSpace(input))
-                    startingBase = int.Parse(input);                    // TODO: Add Exception handling
+                try{
+                        startingBase = int.Parse(input);
+                   }
+                    catch
+                    {
+                        ConsolePrinter("Invalid number as starting base", 0);
+                    }
                     if (startingBase == 0)
                         return;
                  {
@@ -106,26 +118,42 @@ namespace TheAllConverter
                     Console.Write("\n \n");
                 }
                 input = Console.ReadLine();
+                input=input.ToLower();
                 if (!String.IsNullOrWhiteSpace(input))
-                    endingBase = int.Parse(input);                      // TODO: Add Exception handling
+                    try
+                    {                       
+                        endingBase = int.Parse(input);
+                        //if (endingBase > 16)
+                        //    throw Exception e;
+                    }                                        // Exception handling not actually implemented
+                    catch
+                    {
+                        ConsolePrinter("Invalid number as ending base", 0);
+                    }
                     if (endingBase == 0)
                         return;
-
-                
-
-
             } while (goOn); 
   
         }
 
-        static string TenToN(string number, int endBase)
+        static string TenToN(string number, int endBase)  // Partial Ten to N: still not handling decimals
         {
             char[] digits = { '0','1','2','3', '4', '5', '6', '7', '8', '9','a', 'b', 'c', 'd', 'e', 'f' };
             int[] rest = new int[50];
             int i = -1;
             char[] answer= new char[50];
-            int changedN = int.Parse(number);
+            int changedN;
+            try
+            {
+                 changedN = int.Parse(number);
+            }
+            catch
+            {
+                ConsolePrinter("Error: Invalid number input",0);
+                return "";
+            }
             while(changedN>0)
+
             {
                 i++;
                 Console.WriteLine("At i=" + i + " performing " + changedN + "%" + endBase);
@@ -136,21 +164,97 @@ namespace TheAllConverter
             int index = i;
             while(i>=0)
             {
-               // Console.WriteLine("rest[i]: "+rest[i]);
                 answer[index - i] = digits[rest[i]];
-               // Console.WriteLine("answer[i]: "+answer[index - i]);
                 i--;
             }
             string answerS = new string(answer);
             return answerS;
         }
 
-
-        static int PowerUp(int a,int b)
+        static Tuple<string, string> NToTen(string number, string decimals, int startBase)
         {
-            int k = a;
-            for (int i = 1; i < b; i++)
-                k *= a;
+            int i = 0;
+            int[] coNumber = new int[50];
+            int[] coDecimal = new int[50];
+            while (i<number.Length)
+            {
+                if (number[i] <= '9' && number[i] >= '0')
+                    coNumber[i] = int.Parse(number[i].ToString());
+                else
+                    switch(number[i])
+                    {
+                        case 'a': coNumber[i] = 10; break;
+                        case 'b': coNumber[i] = 11; break;
+                        case 'c': coNumber[i] = 12; break;
+                        case 'd': coNumber[i] = 13; break;
+                        case 'e': coNumber[i] = 14; break;
+                        case 'f': coNumber[i] = 15; break;
+                    }
+                i++;
+                
+            }
+            i = 0;
+            while (i < decimals.Length)
+            {
+                if (decimals[i] <= '9' && decimals[i] >= '0')
+                    coDecimal[i] = int.Parse(decimals[i].ToString());
+                else
+                    switch (decimals[i])
+                    {
+                        case 'a': coDecimal[i] = 10; break;
+                        case 'b': coDecimal[i] = 11; break;
+                        case 'c': coDecimal[i] = 12; break;
+                        case 'd': coDecimal[i] = 13; break;
+                        case 'e': coDecimal[i] = 14; break;
+                        case 'f': coDecimal[i] = 15; break;
+                    }
+                //Console.WriteLine(decimals[i]);
+                //Console.WriteLine(coDecimal[i]);
+
+
+                i++;
+            }
+
+            Decimal sumaDec = 0;           
+            i = 0;
+            while(i<decimals.Length)
+            {
+                sumaDec += (decimal)coDecimal[i] / (decimal)PowerUp(startBase, i + 1);
+                i++;
+            }
+            Decimal sumaNb = sumaDec;
+            i = 0;
+            while (i<number.Length)
+            {
+                sumaNb += (decimal)coNumber[i] * (decimal)PowerUp(startBase, number.Length-i-1);
+                i++;
+            }
+            Console.WriteLine(sumaNb); // remove separator from Dec
+            string answer = "" + sumaNb;
+            string nbAnswer="0";
+            string dcAnswer="0";
+            string[] splits = new string[2];
+            if (sumaDec > 0)
+            {
+                splits = answer.Split('.');
+                dcAnswer = "" + splits[1];
+                nbAnswer = "" + splits[0];
+            }
+            else
+                nbAnswer = "" + sumaNb;            
+
+            return Tuple.Create(nbAnswer,dcAnswer);
+        }
+
+        static int PowerUp(int baza,int putere)
+        {
+            int k = 1;
+            int i = 0;
+            while(i<putere)
+            {
+                k *= baza;
+                i++;
+            }
             return k;
         }
 
